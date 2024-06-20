d89c4f7cd53ef2d993ca89494ab6a164284d573c
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-20 03:09:57 +0000

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,14 +1,14 @@
 const { zokou } = require("../framework/zokou");
 const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "fancy", categorie: "Fun", reaction: "📜" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExample : ${prefixe}fancy 10 Kimmy\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('KIMMY', fancy));
+            return await repondre(`\nExample : ${prefixe}fancy 10 Bryant\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('MADARA', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
