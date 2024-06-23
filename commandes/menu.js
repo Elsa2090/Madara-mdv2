8b33403f67760df95f89f0af2dffdaad6f014213
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-23 21:10:24 +0000

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -65,7 +65,7 @@ const date = moment().format('DD/MM/YYYY');
         menuMsg += `*╭────✦* *${cat}* *✦⊷*`;
         for (const cmd of coms[cat]) {
             menuMsg += `
-*𓃵* ${cmd}`;
+*🎁* ${cmd}`;
         }
         menuMsg += `
 *╰═════════════⊷* \n`
